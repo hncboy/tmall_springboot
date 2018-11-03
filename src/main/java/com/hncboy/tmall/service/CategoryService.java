9c9cbc0dafb78ad544e30c786e6b85a20dcf0e19
hncboy <619452863@qq.com>
2018-11-03 20:14:46 +0800

    完成了后台分类的显示

--- src/main/java/com/hncboy/tmall/service/CategoryService.java
+++ src/main/java/com/hncboy/tmall/service/CategoryService.java
@@ -2,7 +2,11 @@ package com.hncboy.tmall.service;
 
 import com.hncboy.tmall.dao.CategoryDAO;
 import com.hncboy.tmall.pojo.Category;
+import com.hncboy.tmall.util.Page4Navigator;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
@@ -19,6 +23,13 @@ public class CategoryService {
 
     @Autowired
     private CategoryDAO categoryDAO;
+
+    public Page4Navigator<Category> list(int start, int size, int navigatePages) {
+        Sort sort = new Sort(Sort.Direction.DESC, "id");
+        Pageable pageable = new PageRequest(start, size, sort);
+        Page pageFromJPA = categoryDAO.findAll(pageable);
+        return new Page4Navigator<Category>(pageFromJPA, navigatePages);
+    }
 
     public List<Category> list() {
         Sort sort = new Sort(Sort.Direction.DESC, "id");
--- src/main/java/com/hncboy/tmall/web/CategoryController.java
+++ src/main/java/com/hncboy/tmall/web/CategoryController.java
@@ -2,11 +2,11 @@ package com.hncboy.tmall.web;
 
 import com.hncboy.tmall.pojo.Category;
 import com.hncboy.tmall.service.CategoryService;
+import com.hncboy.tmall.util.Page4Navigator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 /**
  * Created by IntelliJ IDEA.
@@ -21,7 +21,12 @@ public class CategoryController {
     private CategoryService categoryService;
 
     @GetMapping("/categories")
-    public List<Category> list() {
-        return categoryService.list();
+    public Page4Navigator<Category> list(
+            @RequestParam(value = "start", defaultValue = "0") int start,
+            @RequestParam(value = "size", defaultValue = "5") int size) {
+        start = start < 0 ? 0 : start;
+        //5表示导航分页最多有5个，像 [1,2,3,4,5] 这样
+        Page4Navigator<Category> page = categoryService.list(start, size, 5);
+        return page;
     }
 }
